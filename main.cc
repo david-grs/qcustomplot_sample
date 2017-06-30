28318f60e04e95307e4bab66e9bde53715369477
David G <david.a.grs@gmail.com>
2017-06-30 18:29:24 +0200

    adding Point

--- main.cc
+++ main.cc
@@ -4,15 +4,27 @@
 #include <qpushbutton.h>
 
 
+QColor GenColor()
+{
+    return QColor(std::rand() % 255, std::rand() % 255, std::rand() % 255);
+}
+
+struct Point
+{
+    double x;
+    double y;
+    QColor color;
+};
+
 int main( int argc, char **argv )
 {
     QApplication a( argc, argv );
 
     Ui::Plot plot;
 
-    for(auto x : {std::make_pair(1,2), std::make_pair(4,10), std::make_pair(6,8)})
-        plot.Push(x.first, x.second);
-        
+    for(auto x : {Point{1, 2, GenColor()}, Point{4, 10, GenColor()}, Point{5, 3, GenColor()}, Point{20, 15, GenColor()}})
+        plot.Push(x.x, x.y, x.color);
+
     plot.show();
     return a.exec();
 }
