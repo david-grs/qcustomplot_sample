5c172b1df8151c86d4a0a5edd68cb7b23379b0a8
David G <david.a.grs@gmail.com>
2017-06-30 17:40:45 +0200

    fixing toolchain

--- main.cc
+++ main.cc
@@ -1,42 +1,8 @@
-#include "thirdparty/qcustomplot.h"
+#include "plot.h"
 
 #include <qapplication.h>
 #include <qpushbutton.h>
 
-namespace Ui {
-    
-class Plot : public QCustomPlot
-{
-Q_OBJECT
-
-public:
-    Plot(QWidget* pp = nullptr);
-
-public slots:
-    void OnValue(double v);
-
-private slots:
-    void OnMousePress(QMouseEvent*);
-    void OnMouseWheel(QWheelEvent*);
-
-private:
-    void InitPlotArea();
-
-    QVector<double> mTimeAxis;
-    QCPGraph* mGraph = nullptr;
-};
-
-Plot::Plot(QWidget* pp) :
-    QCustomPlot(pp)
-{
-    InitPlotArea();
-}
-
-void Plot::InitPlotArea()
-{
-}
-
-}
 
 int main( int argc, char **argv )
 {
