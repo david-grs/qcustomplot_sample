8b32be66781270f81e9ac7bb568cb407e376da71
David G <david.a.grs@gmail.com>
2017-06-30 17:38:46 +0200

    Adding Plot

--- main.cc
+++ main.cc
@@ -1,13 +1,48 @@
 #include "thirdparty/qcustomplot.h"
+
 #include <qapplication.h>
 #include <qpushbutton.h>
 
+namespace Ui {
+    
+class Plot : public QCustomPlot
+{
+Q_OBJECT
+
+public:
+    Plot(QWidget* pp = nullptr);
+
+public slots:
+    void OnValue(double v);
+
+private slots:
+    void OnMousePress(QMouseEvent*);
+    void OnMouseWheel(QWheelEvent*);
+
+private:
+    void InitPlotArea();
+
+    QVector<double> mTimeAxis;
+    QCPGraph* mGraph = nullptr;
+};
+
+Plot::Plot(QWidget* pp) :
+    QCustomPlot(pp)
+{
+    InitPlotArea();
+}
+
+void Plot::InitPlotArea()
+{
+}
+
+}
 
 int main( int argc, char **argv )
 {
     QApplication a( argc, argv );
 
-    QCustomPlot plot;
+    Ui::Plot plot;
     plot.show();
     return a.exec();
 }
