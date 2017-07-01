d24b6f3ef36ec99ede97f7da1a8ae780457a9c24
David G <david.a.grs@gmail.com>
2017-07-01 21:55:43 +0200

    cleanup

--- window.h
+++ window.h
@@ -4,43 +4,6 @@
 
 #include <QMainWindow>
 
-#include <iostream>
-#include <cstdlib>
-#include <array>
-
-struct Point
-{
-    double x;
-    double y;
-    QColor color;
-};
-
-inline QColor GenColor()
-{
-    auto color = []() -> int { return std::abs(std::rand()) % 255; };
-    auto alpha = []() -> int { return std::abs(std::rand()) % 64 + 180; };
-    return QColor(color(), color(), color(), alpha());
-}
-
-inline Point GenPoint(double x) { return {x, double(std::rand() % 100), GenColor()}; }
-
-template <std::size_t N>
-struct GenPoints
-{
-    std::array<Point, N> operator()()
-    {
-        return impl(std::make_index_sequence<N>());
-    }
-
-private:
-    template <std::size_t... Is>
-    std::array<Point, N> impl(std::index_sequence<Is...>)
-    {
-        return {GenPoint(Is)...};
-    }
-};
-
-
 namespace Ui {
 
 class Window : public QMainWindow
@@ -48,22 +11,7 @@ class Window : public QMainWindow
     Q_OBJECT
 
 public:
-    Window() :
-        mPlot(this)
-    {
-        int i = 0;
-        for(auto x : GenPoints<10>()())
-        {
-            auto now = QDateTime::currentDateTime().addSecs(-60.0 * (9 - i++));
-            mPlot.Push(now, x.y, x.color);
-        }
-
-        resize(800, 600);
-        setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
-        setCentralWidget(&mPlot);
-
-        mPlot.installEventFilter(this);
-    }
+    explicit Window();
 
 protected:
     bool eventFilter(QObject *obj, QEvent *event) override;
